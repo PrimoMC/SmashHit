7ecb4fa28d98ad7567227b7aa8490d53252a1cae
Luuk Jacobs <kukelekuuk00@radthorne.info>
2016-09-06 12:04:37 +0200

    use the attribute API present in spigot 1.9+

--- src/main/java/com/frash23/smashhit/DamageResolver.java
+++ src/main/java/com/frash23/smashhit/DamageResolver.java
@@ -1,12 +1,9 @@
 package com.frash23.smashhit;
 
-import com.frash23.smashhit.Util.Reflection;
+import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.Damageable;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
-
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 
 public class DamageResolver
 {
@@ -20,24 +17,12 @@ public class DamageResolver
 
     public double getDamage( Player damager, Damageable entity )
     {
-        try
+        double damage = damager.getAttribute( Attribute.GENERIC_ATTACK_DAMAGE ).getValue();
+        if ( USE_CRITS && !( (Entity) damager ).isOnGround() && damager.getVelocity().getY() < 0 && OLD_CRITS || !damager.isSprinting() )
         {
-            Object handle = Reflection.getHandle( damager );
-            Field field = Reflection.getField( Reflection.getNMSClass( "GenericAttributes" ), "ATTACK_DAMAGE" );
-            Object attributeInstance = Reflection.getMethod( handle.getClass(), "getAttributeInstance", new Class[0] ).invoke( handle, new Object[]{ field.get( null ) } );
-            double damage = (double) Reflection.getMethod( attributeInstance.getClass(), "getValue", new Class[0] ).invoke( handle, new Object[0] );
-            if ( USE_CRITS && !( (Entity) damager ).isOnGround() && damager.getVelocity().getY() < 0 && OLD_CRITS || !damager.isSprinting() )
-            {
-                damage *= 1.5;
-            }
-
-            return damage;
+            damage *= 1.5;
         }
-        catch ( IllegalAccessException | InvocationTargetException e )
-        {
-            e.printStackTrace();
-        }
-        return 0;
+        return damage;
     }
 
     DamageResolver( boolean useCrits, boolean oldCrits )
