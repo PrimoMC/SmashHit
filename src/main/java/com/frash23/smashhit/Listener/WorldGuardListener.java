436869f56b3cfb0ed0495dbfd9cda40b161c5e15
Luuk Jacobs <kukelekuuk00@radthorne.info>
2016-09-25 12:40:00 +0200

    Make the worldguard handler also handle mob/animal damage. And fix it so it better mimics default worldguard behaviour. These flags should only block when explicitly on deny. Because they're implicitly true.

--- src/main/java/com/frash23/smashhit/Listener/WorldGuardListener.java
+++ src/main/java/com/frash23/smashhit/Listener/WorldGuardListener.java
@@ -2,9 +2,13 @@ package com.frash23.smashhit.Listener;
 
 import com.frash23.smashhit.Event.AsyncPreDamageEvent;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
 import com.sk89q.worldguard.protection.flags.DefaultFlag;
+import com.sk89q.worldguard.protection.flags.StateFlag;
+import com.sk89q.worldguard.protection.managers.RegionManager;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
+import org.bukkit.entity.Animals;
 import org.bukkit.entity.Damageable;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -26,10 +30,50 @@ public class WorldGuardListener implements Listener
         Damageable entity = e.getEntity();
         World world = damager.getWorld();
 
-        if ( !wg.getRegionManager( world ).getApplicableRegions( damager.getLocation() ).testState( wg.wrapPlayer( damager ), DefaultFlag.PVP ) || !wg.getRegionManager( world ).getApplicableRegions( entity.getLocation() ).testState( wg.wrapPlayer( damager ), DefaultFlag.PVP ) )
+        RegionManager wgrm = wg.getRegionManager( world );
+        StateFlag[] flags;
+
+        if ( entity instanceof Player )
+        {
+            flags = new StateFlag[]{ DefaultFlag.PVP };
+        }
+        else if ( entity instanceof Animals )
+        {
+            flags = new StateFlag[]{ DefaultFlag.DAMAGE_ANIMALS, DefaultFlag.MOB_DAMAGE };
+        }
+        else
+        {
+            flags = new StateFlag[]{ DefaultFlag.MOB_DAMAGE };
+        }
+        if ( !test( wgrm, damager, entity, flags ) )
         {
             e.setCancelled( true );
         }
+    }
+
+    private boolean test( RegionManager wgrm, Player damager, Damageable entity, StateFlag... flag )
+    {
+        ApplicableRegionSet set = wgrm.getApplicableRegions( damager.getLocation() );
+        if ( set.queryState( wg.wrapPlayer( damager ), flag ) == StateFlag.State.DENY )
+        {
+            return false;
+        }
+        set = wgrm.getApplicableRegions( entity.getLocation() );
+        if ( entity instanceof Player )
+        {
+            if ( set.queryState( wg.wrapPlayer( (Player) entity ), flag ) == StateFlag.State.DENY )
+            {
+                return false;
+            }
+        }
+        else
+        {
+            if ( set.queryState( null, flag ) == StateFlag.State.DENY )
+            {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
